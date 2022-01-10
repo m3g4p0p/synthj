86779216938cb2cab7a50bd0a156f249144ef1d6
Sebastian Seitz <bassistsagainstracists@gmx.net>
2022-01-10 09:46:55 +0100

    add overlay

--- public/app.js
+++ public/app.js
@@ -1,9 +1,12 @@
 import { Keyboard } from './keyboard.js'
 import { Synthie } from './synthie.js'
 
-window.addEventListener('click', () => {
+const overlay = document.getElementById('overlay')
+
+overlay.addEventListener('click', () => {
   const keyboard = new Keyboard('keyboard')
   const synthie = new Synthie()
 
   synthie.connect(keyboard)
-}, { once: true })
+  overlay.parentElement.removeChild(overlay)
+})
